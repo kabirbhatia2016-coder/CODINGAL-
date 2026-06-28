9de5ae7eb63338c62781aaf4183fd996379ba45b
Kabir <kabirbhatia2016@gmail.com>
2026-06-28 10:04:29 -0400

    Experimenting with variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+x = 9
+y = "Kabir"
+
+print("My name is", y, "and I am", x, "years old.")
+
+name = input("What is your name? ")
+
+
+print("Hello, " + name + "! Welcome to the world of programming.")
+
+float_num = 3.14
+
+print("The value of pi is approximately", float_num)
