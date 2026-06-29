85b8964ae8382c25275968e5db0995e367164f97
Kabir <kabirbhatia2016@gmail.com>
2026-06-29 18:46:49 -0400

    New Changes

--- Bye_Bye.py
+++ Bye_Bye.py
@@ -1,6 +1,15 @@
-print("Aarav's birthday is on October 29th.")
-print("My Mom's birthday is on September 18th.")
-print("My Dad's birthday is on October 29th.")
-print("My Birthday is on October 27th.")
-birth = input("Enter your birthday (in the format MM-DD): ")
-print("Your birthday is on", birth)
+BrothersBirthday = "October 29th"
+MyBirthday = "October 27th"
+MomsBirthday = "September 18th"
+DadsBirthday = "October 29th"
+FriendsBirthday = "June 24th"
+
+print("My brother's birthday is on " + BrothersBirthday)
+print("My birthday is on " + MyBirthday)
+print("My mom's birthday is on " + MomsBirthday)
+print("My dad's birthday is on " + DadsBirthday)
+print("My friend's birthday is on " + FriendsBirthday)
+
+birth = input ("When is your birthday? (Enter in format: MM/DD) ")
+
+print("Good time to celebrate your birthday on " + birth + "!")
