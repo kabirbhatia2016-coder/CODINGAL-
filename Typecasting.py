e0a04334c4d46bbd26cd564f2cfeeec8ba16ec97
Kabir <kabirbhatia2016@gmail.com>
2026-07-02 19:18:51 -0400

    Experimenting with Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,14 @@
+Age = 363636366363636366363636363636636363636
+Weight = 56.2
+
+print(Age, "The type of this variable is ", type(Age))
+
+print(Weight, "The type of this variable is ", type(Weight))
+
+Weight = int(Weight)
+Age = str(Age)
+
+
+print(Age, "The type of this variable is ", type(Age))
+
+print(Weight, "The type of this variable is ", type(Weight))
