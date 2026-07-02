42b9c05f3bd81a937857bdbe74c09a621fa675e2
Kabir <kabirbhatia2016@gmail.com>
2026-07-02 19:08:29 -0400

    Experimeting with Data Types

---
+++ data_types.py
@@ -0,0 +1,11 @@
+decimals = 10.24
+num1 = 4
+Yes_No = True
+say = "I am really cool"
+
+print(decimals , "The type of this variable is", type(decimals))
+print(say , "The type of this variable is", type(say))
+print(Yes_No , "The type of this variable is", type(Yes_No))
+print(num1 , "The type of this variable is", type(num1))
+
+
