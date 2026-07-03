db7e8ee1d858193d0b87798e965ac1a17b07e61b
Kabir <kabirbhatia2016@gmail.com>
2026-07-02 21:34:27 -0400

    All the String Methods in one file

---
+++ i_hope_its_final.py
@@ -0,0 +1,23 @@
+# Uppercase and Lowercase
+print("congratulations".upper())
+print("CONGRATS".lower())
+Str = "Congratulations"
+
+
+# String Slicing
+str1 = "Python"
+print(str1[0:2])
+print(str1[2:4])
+print(str1[4:6])
+
+#String Concatenation
+str2 = "Python"
+str3 = "is a programming language"
+
+print(str2 + " " + str3)
+
+# String Reversal
+user_input = input("Please input a string and I will reverse it: ")
+reversed_string = user_input[::-1]
+print(reversed_string)
+
